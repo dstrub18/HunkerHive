1ae095f68f51bfb6fc2cb29a61752165366d950a
dstrub18 <dstrub18@student.aau.dk>
2020-01-31 22:41:33 +0100

    Made scrappy wind excitation by modulating the z rotation
    Made scrappy wind excitation by modulating the z rotation

---
+++ HunkerHive/Assets/Scripts/RopeMover.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RopeMover : MonoBehaviour
+{
+    private float angleModulator;
+    [SerializeField] private float angleModulatorMin;
+    [SerializeField] private float angleModulatorMax;
+    [SerializeField] private float smooth;
+
+    private Quaternion target;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        angleModulator = Random.Range (angleModulatorMin, angleModulatorMax);
+        target = Quaternion.Euler(0, 0, 0 + angleModulator);
+
+        transform.rotation = Quaternion.Slerp(transform.rotation, target, Time.deltaTime * smooth);
+
+    }
+}
