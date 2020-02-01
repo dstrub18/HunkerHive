a5be1be5ea9c06250104cd61eb0aae0e583c3e59
dstrub18 <dstrub18@student.aau.dk>
2020-02-01 15:01:16 +0100

    Added forceApplier, sits on Capsule object

---
+++ HunkerHive/Assets/Scripts/ForceApplier.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForceApplier : MonoBehaviour
+{
+    [Range (2, 10)]
+    [SerializeField] private float forceInterval;
+
+    [Space (10)]
+
+    [Range(100, 200)]
+    [SerializeField] private float forceStrength;
+
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private bool applyLeft;
+    [SerializeField] private ForceMode2D fm;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        applyLeft = true;
+        fm = ForceMode2D.Force;
+
+        StartCoroutine(applyForce ());
+    }
+
+    // Coroutine for applying force
+
+     IEnumerator applyForce()
+    {
+        if (applyLeft)
+        {
+            rb.AddForce(-transform.right * forceStrength, fm);
+            Debug.Log("Force applied to the \t \t left");
+            applyLeft = false;
+            yield return new WaitForSeconds(forceInterval);
+        }
+
+        if (!applyLeft)
+        {
+            rb.AddForce(transform.right * forceStrength, fm);
+            Debug.Log("Force applied to the \t \t right");
+            applyLeft = true;
+            yield return new WaitForSeconds(forceInterval);
+
+        }
+
+        }
+
+    }
+
