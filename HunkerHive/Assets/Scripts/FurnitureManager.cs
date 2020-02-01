944d986f7be04ace73c5de11f8784d785df34bbd
Joseph Cherry <joecherrysong@gmail.com>
2020-02-01 13:35:25 +0100

    Grab and drop scripts for BUI

---
+++ HunkerHive/Assets/Scripts/FurnitureManager.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FurnitureManager : MonoBehaviour
+{
+    public FurnitureTrigger furnitureTrigger = null;
+
+}
